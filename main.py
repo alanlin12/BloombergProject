1ce015c070a7818c37dd652e540339666c0eca62
alanlin12 <alanlin3008@gmail.com>
2025-09-29 19:46:06 -0400

    Main Program for FastAPI

---
+++ main.py
@@ -0,0 +1,32 @@
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import yfinance as yf
+
+app = FastAPI(
+    title="Stock Info API",
+    description="Gathers simple stock information"
+)
+
+class stock_ticker(BaseModel):
+    ticker: str
+
+@app.get("/")
+async def root():
+    return {"message": "Stock Information"}
+
+@app.post("/stock/{stock_ticker}")
+async def obtain_stock_data(request: stock_ticker):
+    try:
+        stock = yf.Ticker(request.ticker)
+        info = stock.info
+        
+        return{
+            "ticker": request.ticker.upper(),
+            "name": info.get("longName"),
+            "price": info.get("currentPrice") or info.get("regularMarketPrice"),
+            "previous_close": info.get("previousClose"),
+            "volume": info.get("volume")
+        }
+    except Exception as e:
+        raise HTTPException(status_code=404, detail="Ticker not found")
+        
