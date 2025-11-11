7de241d0a29c8daf58f6e17f18003d852c1c3cd5
alanlin12 <alanlin3008@gmail.com>
2025-11-10 19:35:25 -0500

    gets stock info

---
+++ stock_tracker.py
@@ -0,0 +1,28 @@
+import requests
+import pandas as pd
+
+url = "https://query1.finance.yahoo.com/v1/finance/screener/predefined/saved"
+headers = {"User-Agent": "Mozilla/5.0"}
+
+def get_active_stocks():
+    params = {"scrIds": "most_actives"}
+
+    data = requests.get(url, params=params, headers=headers).json()
+    quotes = data["finance"]["result"][0]["quotes"]
+
+    df = pd.DataFrame(quotes)[["symbol", "shortName", "regularMarketPrice", "regularMarketChangePercent", "trailingPE"]]
+    return df[:10]
+
+def get_gaining_stocks():
+    params = {"scrIds": "day_gainers"}
+    
+    data = requests.get(url, params=params, headers=headers).json()
+    quotes = data["finance"]["result"][0]["quotes"]
+
+    df = pd.DataFrame(quotes)[["symbol", "shortName", "regularMarketPrice", "regularMarketChangePercent", "trailingPE"]]
+    return df[:10]
+
+print(get_active_stocks(), "\n\n")
+print(get_gaining_stocks())
+
+
